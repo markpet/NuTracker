cc5a13b24c9f3403a606860be71b29f8e860369f
Mark Peters <mark@ordersixtyseven.com>
2018-10-09 13:43:27 +0100

    Updates on Main.storyboard

--- Nutracker/Nutracker/ViewController.swift
+++ Nutracker/Nutracker/ViewController.swift
@@ -10,8 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var roundedCornerButton: UIButton!
+    @IBOutlet weak var roundedCornerButton2: UIButton!
+    @IBOutlet weak var roundedCornerButton3: UIButton!
+    @IBOutlet weak var roundedCornerButton4: UIButton!
+    @IBOutlet weak var roundedCornerButton5: UIButton!
+    @IBOutlet weak var roundedCornerButton6: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        roundedCornerButton.layer.cornerRadius = 4
+        roundedCornerButton2.layer.cornerRadius = 4
+        roundedCornerButton3.layer.cornerRadius = 4
+        roundedCornerButton4.layer.cornerRadius = 4
+        roundedCornerButton5.layer.cornerRadius = 4
+        roundedCornerButton6.layer.cornerRadius = 4
         // Do any additional setup after loading the view, typically from a nib.
     }
 
